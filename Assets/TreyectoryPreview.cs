3cb6f66272d87e65f8dba8b8ec461229480e475f
MoonMayra <m.mansilla001@gmail.com>
2025-10-07 11:19:59 -0300

    Add TrayectoryPreview feature and update scene
    Introduces TrayectoryPreview prefab and its supporting script to the project. Updates SampleScene.unity to include the new prefab and reorganizes object hierarchy, replacing some objects and grids to support the new trajectory preview functionality.

---
+++ Assets/TreyectoryPreview.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TreyectoryPreview : MonoBehaviour
+{
+    [SerializeField] private int maxBounces = 3;
+    [SerializeField] private LayerMask bouncesMask;
+
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void DrawTrayectory(Vector2 startPos, Vector2 lineDirection)
+    {
+        List<Vector3> bouncesPoints=new List<Vector3>();
+        bouncesPoints.Add(startPos);
+
+        Vector2 currentPos = startPos;
+        Vector2 currentDir=lineDirection.normalized;
+
+        for (int i = 0; i <= maxBounces; i++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(currentPos, currentDir, Mathf.Infinity, bouncesMask);
+            if (hit.collider)
+            {
+                bouncesPoints.Add(hit.point);
+
+                if (i == maxBounces)
+                {
+                    break;
+                }
+
+                currentDir = Vector2.Reflect(currentDir, hit.normal);
+            }
+            else
+            {
+                bouncesPoints.Add(currentPos + currentDir * 1000f);
+                break;
+            }
+        }
+        lineRenderer.positionCount=bouncesPoints.Count;
+        lineRenderer.SetPositions(bouncesPoints.ToArray());
+
+
+    }
+
+    private void ClearLine()
+    {
+        lineRenderer.positionCount = 0;
+    }
+
+}
