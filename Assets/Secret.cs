6a73c23aeb90aa128201bf5ba42ab11131514658
MoonMayra <m.mansilla001@gmail.com>
2025-10-14 15:10:59 -0300

    Add SecretTile assets and update jungle tiles
    Introduced SecretTile asset, sprite, and associated metadata for the jungle tree palette. Updated Tiles.prefab to include new tile references and expanded the tilemap with additional tiles. Modified Jungle.unity scene and TagManager settings to support new secret tile functionality.

---
+++ Assets/Secret.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Secret : MonoBehaviour
+{
+    [SerializeField] private LayerMask playerLayer;
+    private Tilemap tiles;
+    private bool wasFound = false;
+
+    private void Start()
+    {
+        tiles= GetComponent<Tilemap>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & playerLayer.value) != 0)
+        {
+            wasFound = true;
+        }
+    }
+
+    private void SetOpacity()
+    {
+        Debug.Log("Fading out");
+        Color color = tiles.color;
+        color.a = Mathf.MoveTowards(color.a, 0.0f, 0.1f);
+        Debug.Log("Current alpha: " + color.a); 
+        tiles.color = color;
+    }
+    private void Update()
+    {
+        if (wasFound)
+        {
+            SetOpacity();
+        }
+        if(tiles.color.a<=0.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
