e32efc3857774f4c9b9bb988fbbd1ae0adbd0b52
nico <starsyoru8@gmail.com>
2025-10-22 20:37:42 -0300

    Refactor breakable leaves to new leaf1 asset and animation
    Replaced the old breakable_leaves assets and tilemap setup with a new leaf1 sprite and animation. Updated the Jungle scene to use the new Breakables GameObject structure and adjusted tilemap and collider data accordingly. Added DestructablePlatforms script for new platform behavior.

---
+++ Assets/Script/Environment/Platforms/DestructablePlatforms.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BreakableTilemapPlatform : MonoBehaviour
+{
+    [Header("Destruction Parameters")]
+    [SerializeField] private float destructionDelay = 1.0f;  
+    [SerializeField] private bool destroyPlatform = false;
+
+    private Tilemap tilemap;
+    private bool isBreaking = false;
+
+    private float destructionTimer = 0f;
+
+    private void Awake()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    private void Update()
+    {
+        if (isBreaking)
+        {
+            destructionTimer += Time.deltaTime;
+
+            if (destructionTimer >= destructionDelay)
+            {
+                if (destroyPlatform)
+                {
+                    tilemap.ClearAllTiles(); 
+                }
+
+                isBreaking = false;
+                destructionTimer = 0f;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isBreaking) return;
+
+        if (collision.gameObject.CompareTag("Player"))
+        { 
+            isBreaking = true;
+            destructionTimer = 0f;
+        }
+    }
+}
