d12c47e8cf0465ce9004a9fc7bcd8d35a41db054
nico <starsyoru8@gmail.com>
2025-11-10 12:00:45 -0300

    Refactor platform prefabs and update Gorilla boss scene
    Renamed and reorganized platform prefabs, moving 'Moving_Platform' to 'Trunk' with updated properties and references. Added a new 'leaf1_0' platform prefab. Updated the Gorilla boss scene to use the new Trunk prefab and reorganized scene hierarchy for platforms and breakables. Also moved interactuable art assets to a new decorations directory.

--- Assets/Script/Environment/Platforms/Handcar.cs
+++ Assets/Script/Environment/Platforms/Handcar.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Handcar : MonoBehaviour
@@ -8,33 +9,50 @@ public class Handcar : MonoBehaviour
 
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 2f;
-    [SerializeField] private float moveDistance = 3f;
+    [SerializeField] private Vector2 finalPos = new Vector2(3f, 0f);
 
     private SpriteRenderer spriteRenderer;
-    private bool isActive = false;
+    private Rigidbody2D rigidBody;
+    public bool isActive = false;
     private Vector2 initialPosition;
 
-   private void Start()
+    private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer.sprite = inactiveSprite;
+    }   
+
+    private void Start()
+    {
+        initialPosition = transform.position;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void UpdateHandcar(Vector2 direction)
     {
-        if (collision.CompareTag("Voids"))
+        if (!isActive)
         {
-            isActive = true;
-            UpdateHandcar();
+            StartCoroutine(MoveHandcar(direction));
         }
     }
 
-    private void UpdateHandcar()
+    private void Movement(Vector2 direction)
     {
-        if (isActive)
+        isActive = true;
+        spriteRenderer.sprite = activeSprite;
+
+        Vector2 targetPosition = initialPosition + direction.normalized * moveSpeed;
+
+        rigidBody.MovePosition(targetPosition);
+    }
+
+   private IEnumerator MoveHandcar(Vector2 direction)
+    {
+        while (initialPosition.x < initialPosition.x + finalPos.x || initialPosition.y < initialPosition.y + finalPos.y)
         {
-            spriteRenderer.sprite = activeSprite;
-            initialPosition = transform.position;
-            Vector2 targetPosition = initialPosition + Vector2.right * moveDistance;
+            Debug.Log("Handcar Moving");
+            Movement(finalPos);
         }
+        yield return new WaitForSeconds(0.02f);
     }
 }
--- Assets/Script/Environment/Platforms/HandcarSensors.cs
+++ Assets/Script/Environment/Platforms/HandcarSensors.cs
@@ -6,4 +6,25 @@ public class HandcarSensors : MonoBehaviour
     private Handcar handcar;
     public enum Side { Left, Right }
     public Side sensorSide;
+
+    private void Awake()
+    {
+        handcar = GetComponentInParent<Handcar>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Voids"))
+        {          
+            if (sensorSide == Side.Left)
+            {
+                handcar.UpdateHandcar(Vector2.left);
+            }
+            else if (sensorSide == Side.Right)
+            {
+                handcar.UpdateHandcar(Vector2.right);
+            }
+        }
+    }
+
 }
