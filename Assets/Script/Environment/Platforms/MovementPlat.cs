0fd60502083373ff1bf118a8932c3b8068151228
nico <starsyoru8@gmail.com>
2025-11-15 14:27:26 -0300

    Add new moving platforms and update platform logic
    Introduced new platform prefabs and objects in the Underground scene, including Squaretop and Squarebottom with movement and collision logic. Enhanced MovementPlat script to support both bounce and limit-based movement, added direction and limit visualization, and updated Trunk prefab and level sorting layers for improved platform behavior and rendering. Also cleaned up unused glyphs in the Colectables font asset.

--- Assets/Script/Environment/Platforms/MovementPlat.cs
+++ Assets/Script/Environment/Platforms/MovementPlat.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MovementPlat : MonoBehaviour
 {
@@ -9,8 +10,21 @@ public class MovementPlat : MonoBehaviour
         Up,
         Down
     }
+
+    public enum PlatformType
+    {
+        Bounce,
+        Limits
+    }
+
     [SerializeField] private float speed = 2f;
+
+    [SerializeField] private PlatformType type = PlatformType.Bounce;
+
     [SerializeField] private string bounceTag = "Ground";
+    [SerializeField] private float minLimit;
+    [SerializeField] private float maxLimit;
+
     [SerializeField] public MovementDirection initialDirection = MovementDirection.Right;
 
     private Vector2 direction;
@@ -21,7 +35,9 @@ public class MovementPlat : MonoBehaviour
     {
         platformRigidBody = GetComponent<Rigidbody2D>();
         platformRigidBody.bodyType = RigidbodyType2D.Kinematic;
+       
         startPosition = transform.position;
+        
         direction = initialDirection switch
         {
             MovementDirection.Left => Vector2.left,
@@ -35,13 +51,42 @@ public class MovementPlat : MonoBehaviour
     private void FixedUpdate()
     {
         MovePlatform();
+
+        if (type == PlatformType.Limits)
+        {
+            CheckLimits();
+        }
     }
 
     private void MovePlatform()
     {
         platformRigidBody.linearVelocity = direction * speed;
-
     }
+
+    private void CheckLimits()
+    {
+        if (initialDirection == MovementDirection.Left || initialDirection == MovementDirection.Right)
+        {
+            if (transform.position.x <= minLimit || transform.position.x >= maxLimit)
+            {
+                ChangeDirection();
+            }
+        }
+        else
+        {
+            if (transform.position.y <= minLimit || transform.position.y >= maxLimit)
+            {
+                ChangeDirection();
+            }
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        platformRigidBody.linearVelocity = Vector2.zero;
+        direction *= -1;
+    }
+
     public void ResetPlatform()
     {
         transform.position = startPosition;
@@ -57,11 +102,39 @@ public class MovementPlat : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (type != PlatformType.Bounce)
+        {
+            return;
+        }
+        
         if (collision.collider.CompareTag(bounceTag))
         {
-            platformRigidBody.linearVelocity = Vector2.zero;
-            direction *= -1;
+            ChangeDirection();
         }
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+
+        if (initialDirection == MovementDirection.Left ||
+            initialDirection == MovementDirection.Right)
+        {
+            Vector3 minPos = new Vector3(minLimit, transform.position.y, 0);
+            Vector3 maxPos = new Vector3(maxLimit, transform.position.y, 0);
+
+            Gizmos.DrawSphere(minPos, 0.1f);
+            Gizmos.DrawSphere(maxPos, 0.1f);
+            Gizmos.DrawLine(minPos, maxPos);
+        }
+        else
+        {
+            Vector3 minPos = new Vector3(transform.position.x, minLimit, 0);
+            Vector3 maxPos = new Vector3(transform.position.x, maxLimit, 0);
+
+            Gizmos.DrawSphere(minPos, 0.1f);
+            Gizmos.DrawSphere(maxPos, 0.1f);
+            Gizmos.DrawLine(minPos, maxPos);
+        }
+    }
 }
