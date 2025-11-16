7639d38bea59fd47ef2a45beac29e676759fcbc8
MoonMayra <m.mansilla001@gmail.com>
2025-11-15 22:24:35 -0300

    Add UI panel animations and assets for area selector
    Introduced new animation controllers and animation clips for Cave, Jungle, and Lab panels in the UI. Added 1x and 2x resolution UI element images and presets for area selection. Updated AreaSelector.unity and AreaSelectorManager.cs to integrate new UI assets and animations.

--- Assets/Script/Main_Menu/AreaSelectorManager.cs
+++ Assets/Script/Main_Menu/AreaSelectorManager.cs
@@ -3,28 +3,52 @@ using UnityEngine.SceneManagement;
 
 public class AreaSelectorManager : MonoBehaviour
 {
-    public GameObject loadingScreen;
+    public GameObject areaSelector;
+    public GameObject jungleArea;
+    public GameObject caveArea;
+    public GameObject labArea;
+    [SerializeField] private string triggerName="Death";
+    [SerializeField] private string menuSceneName = "MainMenu";
+    private Animator animator;
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void ChangeAnimation()
+    {
+        animator.SetTrigger(triggerName);
+    }
     public void GoToLevelSelectorJungle()
     {
-        loadingScreen.SetActive(true);
-        UnityEngine.SceneManagement.SceneManager.LoadScene("LevelSelectorJungle");
+        areaSelector.SetActive(false);
+        ChangeAnimation();
+        jungleArea.SetActive(true);
+    }
+    public void GoToLevelSelectorCave()
+    {
+        areaSelector.SetActive(false);
+        ChangeAnimation();
+        caveArea.SetActive(true);
+    }
+    public void GoToLevelSelectorLab()
+    {
+        areaSelector.SetActive(false);
+        ChangeAnimation();
+        labArea.SetActive(true);
+    }
+    public void GoBackToAreaSelector()
+    {
+        jungleArea.SetActive(false);
+        caveArea.SetActive(false);
+        labArea.SetActive(false);
+        ChangeAnimation();
+        areaSelector.SetActive(true);
     }
 
-    public void GoToLevelSelectorUnderground()
-    {
-        loadingScreen.SetActive(true);
-        UnityEngine.SceneManagement.SceneManager.LoadScene("LevelSelectorUnderground");
-    }
-
-    public void BackToAreaSelector()
-    {
-        loadingScreen.SetActive(true);
-        UnityEngine.SceneManagement.SceneManager.LoadScene("AreaSelector");
-    }
     public void BackToMainMenuScene()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(menuSceneName);
     }
 
 }
