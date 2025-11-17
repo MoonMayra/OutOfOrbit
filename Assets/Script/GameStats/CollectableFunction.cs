8122127192ad5e4a68947c1f581e02fb8a0a435b
WykaG <micaelaavrilgiordanino@gmail.com>
2025-11-17 14:06:56 -0300

    Add collectable sound effect and pitch variation
    Introduces a new collectable sound effect and integrates it into the Collectable prefab and CollectableFunction script. The sound is played with random pitch variation when a collectable is obtained, enhancing feedback. Also updates font asset to support additional glyphs.

--- Assets/Script/GameStats/CollectableFunction.cs
+++ Assets/Script/GameStats/CollectableFunction.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class CollectableFunction : MonoBehaviour
 {
-
     [SerializeField] private LayerMask player;
     [SerializeField] private LevelManager levelManager;
     private Collectable collectable;
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip collectSound;
+
+    [Header("Pitch Settings")]
+    [SerializeField] private float minPitch = 0.9f;
+    [SerializeField] private float maxPitch = 1.1f;
 
     private void Awake()
     {
         collectable = GetComponent<Collectable>();
     }
+
     private void Start()
     {
         levelManager = LevelManager.Instance;
@@ -18,9 +26,17 @@ public class CollectableFunction : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(((1<<collision.gameObject.layer) & player.value) != 0)
+        if (((1 << collision.gameObject.layer) & player.value) != 0)
         {
             Debug.Log("Collectable obtained");
+
+            // Pitch variation for collect sound
+            if (audioSource != null && collectSound != null)
+            {
+                audioSource.pitch = Random.Range(minPitch, maxPitch);
+                audioSource.PlayOneShot(collectSound);
+            }
+
             levelManager.GetComponent<PlayerStats>().AddCollectable(1);
             collectable.OnCollected();
         }
